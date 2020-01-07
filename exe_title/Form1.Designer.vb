152bf1fdb04e8ef174b275d0d10679f2bb4d51d7
Raka-loah <i@lotc.cc>
2020-01-07 11:41:23 +0800

    添加项目文件。

---
+++ exe_title/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(295, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(235, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "播放软件"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "原始标题格式"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "输出标题格式"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "最大长度"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(561, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 129)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "▶ 启动"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(126, 21)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(163, 20)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(126, 129)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(404, 21)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(27, 132)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 12)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "输出预览"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 500
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(307, 105)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 12)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "滚动间隔（毫秒）"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(582, 159)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(53, 12)
+        Me.LinkLabel1.TabIndex = 10
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "使用说明"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(206, 26)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(205, 22)
+        Me.ToolStripMenuItem1.Text = "重置所有设置为默认值..."
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(27, 159)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 12)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "输出文件路径"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.DataBindings.Add(New System.Windows.Forms.Binding("Value", Global.exe_title.My.MySettings.Default, "MarqueeInterval", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.NumericUpDown2.Increment = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.NumericUpDown2.Location = New System.Drawing.Point(410, 102)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(120, 21)
+        Me.NumericUpDown2.TabIndex = 6
+        Me.NumericUpDown2.Value = Global.exe_title.My.MySettings.Default.MarqueeInterval
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = Global.exe_title.My.MySettings.Default.Marquee
+        Me.CheckBox1.DataBindings.Add(New System.Windows.Forms.Binding("Checked", Global.exe_title.My.MySettings.Default, "Marquee", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.CheckBox1.Location = New System.Drawing.Point(252, 104)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "滚动"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.DataBindings.Add(New System.Windows.Forms.Binding("Value", Global.exe_title.My.MySettings.Default, "MaxLength", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.NumericUpDown1.Location = New System.Drawing.Point(126, 102)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {320, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 21)
+        Me.NumericUpDown1.TabIndex = 4
+        Me.NumericUpDown1.Value = Global.exe_title.My.MySettings.Default.MaxLength
+        '
+        'TextBox5
+        '
+        Me.TextBox5.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.exe_title.My.MySettings.Default, "OutputFilePath", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox5.Location = New System.Drawing.Point(126, 156)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(404, 21)
+        Me.TextBox5.TabIndex = 8
+        Me.TextBox5.Text = Global.exe_title.My.MySettings.Default.OutputFilePath
+        '
+        'TextBox3
+        '
+        Me.TextBox3.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.exe_title.My.MySettings.Default, "CustomPattern", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox3.Location = New System.Drawing.Point(126, 75)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(404, 21)
+        Me.TextBox3.TabIndex = 3
+        Me.TextBox3.Text = Global.exe_title.My.MySettings.Default.CustomPattern
+        '
+        'TextBox2
+        '
+        Me.TextBox2.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.exe_title.My.MySettings.Default, "RawPattern", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox2.Location = New System.Drawing.Point(126, 48)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(404, 21)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = Global.exe_title.My.MySettings.Default.RawPattern
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "请选择放置music_title.txt的文件夹"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(683, 195)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Process Title Extractor"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label6 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+End Class
---
+++ exe_title/Form1.vb
@@ -0,0 +1,170 @@
+﻿Imports System.ComponentModel
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim last_music_title As String = ""
+    Dim last_write As String = ""
+    Dim proc_title As String = ""
+    Dim music_players As New Dictionary(Of String, String)
+    Dim processes As Process()
+    Dim marquee_index As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        music_players.Add("网易云音乐", "cloudmusic")
+        music_players.Add("QQ音乐", "qqmusic")
+        music_players.Add("Foobar2000", "foobar2000")
+        music_players.Add("其他（右侧填写）", My.Settings.CustomExecutable)
+        ComboBox1.DisplayMember = "Key"
+        ComboBox1.ValueMember = "Value"
+        ComboBox1.DataSource = New BindingSource(music_players, Nothing)
+        ComboBox1.SelectedIndex = My.Settings.SelectedIndex
+        Timer2.Interval = NumericUpDown2.Value
+    End Sub
+
+    ''' <summary>
+    ''' Extract info from given window title.
+    ''' </summary>
+    ''' <param name="exe_title">Window title</param>
+    ''' <param name="pattern">Pattern using %description% format</param>
+    ''' <returns>Dictionary of %description%: matched content</returns>
+    Private Function get_exe_title(exe_title As String, pattern As String) As Dictionary(Of String, String)
+        Dim dic As New Dictionary(Of String, String)
+        'Convert raw pattern string
+        Dim converted_pattern As String = Regex.Replace(Regex.Escape(pattern), "\%(.+?)\%", "(?<$1>.+)", RegexOptions.IgnoreCase)
+        Dim re = New Regex(converted_pattern)
+        Dim results = re.Match(exe_title)
+        If results.Success Then
+            For i = 1 To results.Groups().Count - 1
+                dic.Add(results.Groups(i).Name, results.Groups(i).Value)
+            Next
+        End If
+        get_exe_title = dic
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TypeOf sender Is Timer Then
+            Timer1.Enabled = False
+        Else
+            Timer1.Enabled = Not Timer1.Enabled
+        End If
+        TextBox2.Enabled = Not Timer1.Enabled
+        TextBox3.Enabled = Not Timer1.Enabled
+        TextBox4.Enabled = Not Timer1.Enabled
+        Button1.Text = If(Timer1.Enabled, "⏹ 停止", "▶ 启动")
+        If Timer1.Enabled Then
+            processes = Process.GetProcessesByName(TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            If processes.Count = 0 Then
+                Throw New ArgumentException("No such process")
+            End If
+            For Each proc As Process In processes
+                proc.Refresh()
+                If (proc.MainWindowTitle.Length > 0) Then
+                    proc_title = proc.MainWindowTitle
+                    Exit For
+                End If
+            Next
+        Catch ex As Exception
+            Debug.Print(ex.Message)
+            TextBox4.Text = ""
+            last_music_title = ""
+            last_write = ""
+            Call Button1_Click(Timer1, New EventArgs)
+            Exit Sub
+        End Try
+        Debug.Print(proc_title)
+        Try
+            Dim dic As Dictionary(Of String, String) = get_exe_title(proc_title, TextBox2.Text)
+            Dim result = TextBox3.Text
+            For Each k In dic.Keys
+                result = result.Replace("%" & k & "%", dic(k)) '*giggles*
+            Next
+            If Not Timer2.Enabled Then
+                TextBox4.Text = If(result.Length > NumericUpDown1.Value, result.Substring(0, NumericUpDown1.Value), result)
+            End If
+            If last_music_title <> result Then
+                marquee_index = 0
+                last_music_title = result
+                If Not Timer2.Enabled And last_write <> TextBox4.Text Then
+                    Try
+                        IO.File.WriteAllText(TextBox5.Text, TextBox4.Text)
+                    Catch ex As Exception
+                        MsgBox(ex.Message, vbOKOnly + vbCritical, "错误")
+                        Timer2.Enabled = False
+                        CheckBox1.Checked = False
+                        TextBox4.Text = ""
+                        last_music_title = ""
+                        last_write = ""
+                        Call Button1_Click(Timer1, New EventArgs)
+                        Exit Sub
+                    End Try
+                End If
+            End If
+        Catch ex As Exception
+            TextBox4.Text = ""
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        TextBox1.Text = ComboBox1.SelectedValue
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If ComboBox1.SelectedIndex = ComboBox1.Items.Count - 1 Then
+            My.Settings.CustomExecutable = TextBox1.Text
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        My.Settings.SelectedIndex = ComboBox1.SelectedIndex
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If marquee_index > last_music_title.Length - NumericUpDown1.Value Then marquee_index = 0
+        If TextBox4.Text.Length >= NumericUpDown1.Value Then
+            TextBox4.Text = last_music_title.Substring(marquee_index, NumericUpDown1.Value)
+            marquee_index += 1
+        Else
+            TextBox4.Text = last_music_title
+        End If
+        Try
+            IO.File.WriteAllText(TextBox5.Text, TextBox4.Text)
+        Catch ex As Exception
+            MsgBox(ex.Message, vbOKOnly + vbCritical, "错误")
+            Timer2.Enabled = False
+            CheckBox1.Checked = False
+        End Try
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Timer2.Enabled = CheckBox1.Checked
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        Timer2.Interval = NumericUpDown2.Value
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://lotc.cc/process-title-extractor.wtf")
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        If MsgBox("是否将所有设置重置为默认值？", vbYesNo + vbQuestion, "确认") = vbYes Then
+            My.Settings.Reset()
+            My.Settings.Save()
+            Application.Restart()
+        End If
+    End Sub
+
+    Private Sub TextBox5_Click(sender As Object, e As EventArgs) Handles TextBox5.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox5.Text = IO.Path.Combine(FolderBrowserDialog1.SelectedPath, "music_title.txt")
+        End If
+    End Sub
+End Class
